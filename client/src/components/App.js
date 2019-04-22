9299728812108d080a6075652f1f4d1c44243db3
Johan <joh.pik.fr@gmail.com>
2019-04-22 15:51:27 +1000

    Created main components & Routing react-router-DOM

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react"
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-class App extends Component {
-  render(){
-    return (
-      <div>APP</div>
-    )
-  }
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
+
+// class App extends Component {
+//   render(){
+//     return (
+//
+//       <div>APP</div>
+//     )
+//   }
+// }
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/show" exact component={StreamShow} />
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ client/src/components/streams/StreamList.js
@@ -0,0 +1,21 @@
+// import React, { Component } from "react"
+// import { BrowserRouter, Route, Link } from 'react-router-dom'
+//
+// class StreamList extends Component {
+//   render(){
+//     return (
+//       <div>StreamList</div>
+//     )
+//   }
+// }
+
+
+import React from "react"
+
+const StreamList = () => {
+  return (
+    <div>StreamList</div>
+  )
+}
+
+export default StreamList

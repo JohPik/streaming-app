01e463eee48e59af64d35c65ab2eebd191d61d7c
Johan <joh.pik.fr@gmail.com>
2019-05-05 16:36:20 +1000

    Fix Typos in Action index file

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -45,7 +45,7 @@ export const fetchStreams = () => {
 
 export const fetchStream = (id) => {
   return async dispatch => {
-    const response = await streams.get(`/streams/{$id}`)
+    const response = await streams.get(`/streams/${id}`)
 
     dispatch( {type: FETCH_STREAM, payload: response.data })
   }
@@ -53,7 +53,7 @@ export const fetchStream = (id) => {
 
 export const editStream = (id, formValues ) => {
   return async dispatch => {
-    const response = await streams.put(`/streams/{$id}`, formValues)
+    const response = await streams.put(`/streams/${id}`, formValues)
 
     dispatch( {type: EDIT_STREAM, payload: response.data })
   }
@@ -61,7 +61,7 @@ export const editStream = (id, formValues ) => {
 
 export const deleteStream = (id) => {
   return async dispatch => {
-    await streams.delete(`/streams/{$id}`)
+    await streams.delete(`/streams/${id}`)
 
     dispatch( {type: DELETE_STREAM, payload: id })
   }

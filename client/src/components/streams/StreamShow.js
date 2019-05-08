cb958595dd5ad32c83650b69a54b12378b8de5f2
Johan <joh.pik.fr@gmail.com>
2019-05-08 21:05:06 +1000

    StreamShow Fetch and Display Title & Description

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,9 +1,37 @@
-import React from "react"
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { fetchStream } from '../../actions'
 
-const StreamShow = () => {
-  return (
-    <div>StreamShow</div>
-  )
+
+class StreamShow extends Component {
+
+  componentDidMount(){
+    if (!this.props.stream) {
+      const id = this.props.match.params.id
+      return this.props.fetchStream(id)
+    }
+    return
+  }
+
+
+  render(){
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+
+    let { title, description } = this.props.stream
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    )
+  }
+
 }
 
-export default StreamShow
+const mapStateToProps = (state, ownProps) => {
+  return ({stream: state.streams[ownProps.match.params.id]})
+}
+export default connect(mapStateToProps, {fetchStream})(StreamShow)
